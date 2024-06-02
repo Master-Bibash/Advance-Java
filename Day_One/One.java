f45af846cd801c3ddc7d25b813429e24d5ba3f5e
Master-Bibash <dokfreak142@gmail.com>
2024-06-02 20:14:19 +0545

    first file and day

---
+++ Day_One/One.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+public class One{
+     public static void main(String[] args) {
+          JFrame myFrame=new JFrame();
+          myFrame.setVisible(true);
+          myFrame.setSize(400,300);
+          myFrame.setTitle("This is a text");
+          myFrame.setResizable(false);
+          myFrame.setLocation(500,200);
+          
+         
+         
+     }
+}
