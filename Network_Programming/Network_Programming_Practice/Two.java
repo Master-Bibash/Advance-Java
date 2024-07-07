6e08c1cbf3ff8a4fbaed6239a4edd56df47d6ae7
Bibash Karki <111418772+Master-Bibash@users.noreply.github.com>
2024-07-07 20:27:18 +0545

    dev (#5)
    * Create README.md
    
    * master

---
+++ Network_Programming/Network_Programming_Practice/Two.java
@@ -0,0 +1,20 @@
+
+import java.io.IOException;
+import java.net.InetAddress;
+
+class Two {
+
+    public static void main(String[] args) throws IOException {
+        try {
+            InetAddress address = InetAddress.getByName("192.168.1.1");
+            if (address instanceof java.net.Inet4Address) {
+                System.out.println("the address is instance of int4address");
+
+            }
+            System.out.println("get ip address is " + address.getAddress());
+        } catch (Exception e) {
+            System.out.println("");
+        }
+    }
+
+}
---
+++ Network_Programming/Network_Programming_Practice/one.java
@@ -0,0 +1,29 @@
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+class one {
+
+    public static void main(String[] args) throws IOException {
+        try {
+            InetAddress address = InetAddress.getByName("www.example.com");
+            boolean isRacable = address.isReachable(1);
+            System.out.println("is reachle is " + isRacable);
+
+            InetAddress localHost = InetAddress.getLocalHost();
+            String ipAdrress = address.getHostAddress();
+            System.out.println("in string ip is " + ipAdrress);
+            byte[] ipAddressInBytes = address.getAddress();
+            System.out.println("in byte " + ipAddressInBytes);
+            System.out.println("our local");
+            System.out.println("address is" + address + "and localhost is" + localHost);
+
+            String gethostName = address.getHostName();
+            System.out.println("the hostname is" + gethostName);
+
+        } catch (UnknownHostException e) {
+            // Handle exception
+        }
+    }
+}
